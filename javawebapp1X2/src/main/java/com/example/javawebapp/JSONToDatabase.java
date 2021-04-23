e89aca4f1ad04c6f8d6fba8ea27fde4a1806b64b
benedwards92 <83072476+benedwards92@users.noreply.github.com>
2021-04-23 13:30:00 +0100

    Add files via upload

--- javawebapp1X2/src/main/java/com/example/javawebapp/JSONToDatabase.java
+++ javawebapp1X2/src/main/java/com/example/javawebapp/JSONToDatabase.java
@@ -27,6 +27,51 @@ public class JSONToDatabase {
         return conn;
     }
 
+    /**
+     * main method that converts json data to database
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception{
+        JSONParser jsonParser = new JSONParser();
 
+        try {
+            JSONObject jsonObject = (JSONObject) jsonParser.parse(new FileReader("bets.json"));
+            JSONArray jsonArray = (JSONArray) jsonObject.get("bets");
+            Connection conn = getConnection();
+            PreparedStatement ps = conn.prepareStatement("INSERT INTO bettingsummary values (?,?,?,?,?,?,?)");
+
+            for(Object object : jsonArray){
+
+                JSONObject record = (JSONObject) object;
+
+                //get variables from json object
+                int id = Integer.parseInt((String) record.get("id"));
+                int numbets = Integer.parseInt((String) record.get("numbets"));
+                String game = (String) record.get("game");
+                float stake = Float.parseFloat((String) record.get("stake"));
+                float returns = Float.parseFloat((String) record.get("returns"));
+                int clientID = Integer.parseInt((String) record.get("clientid"));
+                String date = (String) record.get("date");
+
+                //set variables to the prepared statement to enter into database
+                ps.setInt(1,id);
+                ps.setInt(2,numbets);
+                ps.setString(3,game);
+                ps.setFloat(4,stake);
+                ps.setFloat(5,returns);
+                ps.setInt(6,clientID);
+                ps.setString(7,date);
+
+                System.out.println("Database updated with new records...");
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (java.lang.Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
 
